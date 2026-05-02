6eb18b4bd17a2cba007ddd1d400dd774c94ceb05
adama4o4 <adamoao44@outlook.com>
2026-05-02 16:48:22 -0400

    Update external.py

--- external.py
+++ external.py
@@ -1,17 +1,22 @@
 """
-Wrapper for file class, to be implemented 
+Wrapper for file class
 """
 
 class Reader:
 
-	def __init__(self, filename):
-		self.__fil = open(filename, "r")
-
+	def __init__(self, name="text.dat"):
+		try:
+			self.__fil = open(name, "r+")
+		except:
+			self.__fil = open(name, "w+")
+			self.__fil.close()
+			self.__fil = open(name, "r+")
 	def read(self, r):
 		return self.__fil.read(r)
+	def write(self, dat):
+		self.__fil.write(dat)
 	def reset(self):
 		return self.__fil.seek(0, 0)
-
 """
 Scanns machine (localhost/127.0.0.1) for open pipes, to be impemented
 """		
